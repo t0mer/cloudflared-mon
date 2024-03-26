f5096eb0ae14a1c56c1d8b83f7b38c1e3e5f3602
Tomer Klein <tomer.klein@gmail.com>
2024-03-26 20:09:47 +0200

    Fix bug with authentication

--- app/app.py
+++ app/app.py
@@ -14,10 +14,9 @@ from sqliteconnector import SqliteConnector
 class Monitor():
     def __init__(self):
         self.token = os.getenv("CF_TOKEN")
-        self.email = os.getenv("CF_EMAIL")
         self.account_id = os.getenv("CF_ACCOUNT_ID") 
         self.url = "https://api.cloudflare.com/client/v4/accounts/" + self.account_id + "/cfd_tunnel?is_deleted=false"
-        self.headers = {"X-Auth-Email":self.email, "Authorization": "Bearer " + self.token}   
+        self.headers = {"Authorization": "Bearer " + self.token}   
         self.connector = SqliteConnector()  
         self.connector.create_tables()
         self.notifires = os.getenv("NOTIFIERS")
@@ -43,28 +42,32 @@ class Monitor():
 
     def check_tunnels_status(self):
         try:
-            # logger.info("Checking Tunnels Status")
+            logger.info("Checking Tunnels Status")
             response = requests.get(monitor.url,headers = monitor.headers)
-            for t in response.json()["result"]:
-                TunnelId = str(t["id"])
-                CurrentTunnelState = t["status"]
-                LastChanged = datetime.now()
-                if not self.connector.is_tunnel_monitored(TunnelId):
-                    self.connector.add_monitored_tunnel(TunnelId,CurrentTunnelState,LastChanged)
-                else:
-                    TunnelState = self.connector.get_monitored_tunnel_by_id(TunnelId)[0][1]
-                    if CurrentTunnelState == "healthy":
-                        icon = "✅"
+            if response.status_code == 200:
+                for t in response.json()["result"]:
+                    TunnelId = str(t["id"])
+                    CurrentTunnelState = t["status"]
+                    LastChanged = datetime.now()
+                    if not self.connector.is_tunnel_monitored(TunnelId):
+                        self.connector.add_monitored_tunnel(TunnelId,CurrentTunnelState,LastChanged)
                     else:
-                        icon = "❌"
-                    if(CurrentTunnelState != TunnelState):
-                        self.send_notification(
-                            title="Tunnel status changed",
-                            message = icon + " The tunnel " + t["name"] + " Status changed from " + TunnelState +" to " + CurrentTunnelState + " " + icon
-                        )
-                        self.connector.update_tunnel_state(TunnelId,CurrentTunnelState,LastChanged)
+                        TunnelState = self.connector.get_monitored_tunnel_by_id(TunnelId)[0][1]
+                        if CurrentTunnelState == "healthy":
+                            icon = "✅"
+                        else:
+                            icon = "❌"
+                        if(CurrentTunnelState != TunnelState):
+                            self.send_notification(
+                                title="Tunnel status changed",
+                                message = icon + " The tunnel " + t["name"] + " Status changed from " + TunnelState +" to " + CurrentTunnelState + " " + icon
+                            )
+                            self.connector.update_tunnel_state(TunnelId,CurrentTunnelState,LastChanged)
+            else:
+                logger.error(f"Error login to cloudflare api with status code {str(response.status_code)} \n {response.text}")
         except Exception as e:
             logger.error("Error chacking tunnels state: " + str(e))
+            
 
 
 
